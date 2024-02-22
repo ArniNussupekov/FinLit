9a3dd17e564a4e31b3bd164cb0c260eb06a6866c
arni <arnibekcs@gmail.com>
2024-02-22 22:10:41 +0600

    Allow models be null

--- user/models.py
+++ user/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255, blank=True)
+    surname = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(max_length=254)
-    phone = models.CharField(max_length=10, blank=True)
+    phone = models.CharField(max_length=10, blank=True, null=True)
     password = models.CharField(max_length=255)
-    age = models.IntegerField(blank=True)
+    age = models.IntegerField(blank=True, null=True)
