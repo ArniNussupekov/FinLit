6dab7a276e813b8032b58322e1c942326debb6f2
arni <arnibekcs@gmail.com>
2024-03-19 22:49:18 +0500

    add pagination to quizzes

--- course/views/quiz.py
+++ course/views/quiz.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -7,12 +7,25 @@ from user.models import User
 from course.serializers import QuizSerializer, QuizProgressSerializer, LeaderBoardSerializer, QuizAnswerSerializer
 
 
+class QuizPagination(pagination.PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class QuizViewSet(viewsets.ViewSet):
+    pagination_class = QuizPagination
+
     def list(self, request):
         course_id = request.query_params.get("course_id")
         quizzes = QuizModel.objects.filter(course_id=course_id)
 
-        return Response(QuizSerializer(quizzes, many=True).data)
+        paginator = self.pagination_class()
+        paginated_quizzes = paginator.paginate_queryset(quizzes, request)
+
+        serializer = QuizSerializer(paginated_quizzes, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
 
     def create(self, request):
         serializer = QuizSerializer(data=request.data)
