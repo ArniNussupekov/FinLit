0209933b2db339ace4cce6e17dd6317fe91228d4
arni <arnibekcs@gmail.com>
2024-03-29 18:34:03 +0500

    fix(): adding new field

--- simulator/models.py
+++ simulator/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class FinancialTrialModel(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    level = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    question = models.CharField(max_length=500)
+    name = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, null=True)
+    level = models.CharField(max_length=255, null=True)
+    category = models.CharField(max_length=255, null=True)
+    question = models.CharField(max_length=500, null=True)
+    situation = models.CharField(max_length=500, null=True)
 
 
 class FinancialTrialAnswerModel(models.Model):
