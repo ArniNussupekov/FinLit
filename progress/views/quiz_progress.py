a3178136aaea80c92f044c5c20f8cf628ceecbe4
arni <arnibekcs@gmail.com>
2024-06-12 02:24:08 +0500

    asd

--- progress/views/quiz_progress.py
+++ progress/views/quiz_progress.py
@@ -43,12 +43,12 @@ class QuizProgressViewSet(viewsets.ViewSet):
 
         course_progress = CourseProgress.objects.filter(Q(course_id=course.id) & Q(user_id=user.id)).first()
         if course_progress is None:
-            return Response({"DidNotJoinedCourse": True})
+            raise 'Did not joined'
 
         checker = QuizProgress.objects.filter(Q(course_id=course.id) & Q(user_id=user.id))
         if checker:
-            return Response({"QuizSubmitted": True})
-        return course.id
+            raise 'Quiz Submited'
+        return course
 
     @classmethod
     def upgrade_balance(cls, user_id):
@@ -70,7 +70,7 @@ class QuizProgressViewSet(viewsets.ViewSet):
         grade = CalculatePercentage.calculate_grade(request.data["answers"])
 
         # Saving progress
-        data = {"course_id": course, "user_id": user_id, "grade": grade, "user_choices": request.data["answers"]}
+        data = {"course_id": course.id, "user_id": user_id, "grade": grade, "user_choices": request.data["answers"]}
         serializer = QuizProgressSerializer(data=data)
         serializer.is_valid()
         serializer.save()
